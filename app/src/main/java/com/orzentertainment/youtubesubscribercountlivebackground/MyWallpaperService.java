ad4ec834a3304a2e0ce8dd4cd3b047cb0c9ff814
Omar <omar2535@gmail.com>
2018-12-25 11:54:48 -0800

    settings up wallpaper service

---
+++ app/src/main/java/com/orzentertainment/youtubesubscribercountlivebackground/MyWallpaperService.java
@@ -0,0 +1,12 @@
+package com.orzentertainment.youtubesubscribercountlivebackground;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyWallpaperService extends android.service.wallpaper.WallpaperService {
+
+    @Override
+    public Engine onCreateEngine() {
+        return null;
+    }
+
+}
