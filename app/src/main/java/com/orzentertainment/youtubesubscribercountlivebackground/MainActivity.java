47351158025c3a5578a9bbaf0a4b0ff4aad9b7a5
Omar <omar2535@gmail.com>
2018-12-25 19:51:52 -0800

    Working drawer

--- app/src/main/java/com/orzentertainment/youtubesubscribercountlivebackground/MainActivity.java
+++ app/src/main/java/com/orzentertainment/youtubesubscribercountlivebackground/MainActivity.java
@@ -1,7 +1,13 @@
 package com.orzentertainment.youtubesubscribercountlivebackground;
 
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button setWallpaperButton = findViewById(R.id.button);
+        setWallpaperButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setCounter(v);
+            }
+        });
+
+    }
+    public void setCounter(View v){
+        Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(this, MyWallpaperService.class));
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/orzentertainment/youtubesubscribercountlivebackground/MyWallpaperService.java
+++ app/src/main/java/com/orzentertainment/youtubesubscribercountlivebackground/MyWallpaperService.java
@@ -1,12 +1,63 @@
 package com.orzentertainment.youtubesubscribercountlivebackground;
+import android.os.Handler;
+import android.service.wallpaper.WallpaperService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.graphics.Canvas;
+import android.graphics.Color;
 
-public class MyWallpaperService extends android.service.wallpaper.WallpaperService {
+public class MyWallpaperService extends WallpaperService {
 
     @Override
     public Engine onCreateEngine() {
-        return null;
+        return new MyWallpaperEngine();
     }
+
+    private class MyWallpaperEngine extends Engine {
+        private final Handler handler = new Handler();
+        private SurfaceHolder holder;
+        private boolean visible;
+        private final Runnable drawRunner = new Runnable() {
+            @Override
+            public void run() {
+                draw();
+            }
+        };
+
+        //Called when the surface is created
+        @Override
+        public void onSurfaceCreated(SurfaceHolder holder) {
+            super.onSurfaceCreated(holder);
+            //call the draw method
+            // this is where you must call your draw code
+            draw();
+        }
+
+        private void draw() {
+            holder = getSurfaceHolder();
+            Canvas canvas = null;
+            try {
+                canvas = holder.lockCanvas();
+                if (canvas != null) {
+                    draw(canvas);
+                }
+            } finally {
+                if (canvas != null)
+                    holder.unlockCanvasAndPost(canvas);
+            }
+            handler.removeCallbacks(drawRunner);
+            if (visible) {
+                handler.postDelayed(drawRunner, 200);
+            }
+        }
+
+        private void draw(Canvas canvas) {
+            canvas.drawColor(Color.parseColor("#C0C0C0"));
+        }
+    }
+
+
+
 
 }
